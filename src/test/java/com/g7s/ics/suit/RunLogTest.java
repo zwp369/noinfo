5bdf8a87475858a2ca7309e31fa970232e6a2905
wenping.zhang <369842410@qq.com>
2020-08-11 10:24:21 +0800

    增加suite 文件，修改了pom文件

---
+++ src/test/java/com/g7s/ics/suit/RunLogTest.java
@@ -0,0 +1,21 @@
+package com.g7s.ics.suit;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+/**
+ * @Author: zhangwenping
+ * @Description:
+ * @Date: Create in 20:42 2020-08-10
+ */
+@RunWith(JUnitPlatform.class)
+@SelectPackages({
+        "com.g7s.ics.testcase.ListManagement",
+        "com.g7s.ics.demo"
+})
+
+public class RunLogTest {
+
+}
+

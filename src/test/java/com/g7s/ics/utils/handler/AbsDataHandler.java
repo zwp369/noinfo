cfe96327ff8c04a513de7de431d869b8f7336001
wenping.zhang <369842410@qq.com>
2020-09-18 16:02:26 +0800

    注释暂时不使用的

--- src/test/java/com/g7s/ics/utils/handler/AbsDataHandler.java
+++ src/test/java/com/g7s/ics/utils/handler/AbsDataHandler.java
@@ -1,61 +1,61 @@
-package com.g7s.ics.handler;
-
-import com.alibaba.fastjson.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Set;
-
-/**
- * @Author: zhangwenping
- * @Description:
- * @Date: Create in 20:06 2020-09-08
- */
-public abstract class AbsDataHandler implements DataHandler {
-
-
-    private String jsonStr;
-
-    public String getJsonStr() {
-        return jsonStr;
-    }
-
-    public void setJsonStr(String jsonStr) {
-        this.jsonStr = jsonStr;
-    }
-
-    void replace(JSONObject jsonObject, DataHandler dataHandler) {
-        Set<String> keys = jsonObject.keySet();
-        for (String key : keys) {
-            Object obj = jsonObject.get(key);
-            if (obj instanceof JSONObject) {
-                replace((JSONObject) obj, dataHandler);
-            } else {
-                dataHandler.handler(jsonObject, key);
-            }
-        }
-    }
-
-
-
-    @Override
-    public void service() {
-        JSONObject jsonObject=null;
-        try {
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(filePath())));
-            for (String line = br2.readLine(); line != null; line = br2.readLine()) {
-                jsonObject = JSONObject.parseObject(line);
-                replace(jsonObject,this);
-                jsonStr=jsonObject.toJSONString();
-                // getProducer().send(topic(), JSON.toJSONString(jsonObject, SerializerFeature.WriteMapNullValue), getImei());
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-
-
-}
+//package com.g7s.ics.handler;
+//
+//import com.alibaba.fastjson.JSONObject;
+//
+//import java.io.BufferedReader;
+//import java.io.FileInputStream;
+//import java.io.InputStreamReader;
+//import java.util.Set;
+//
+///**
+// * @Author: zhangwenping
+// * @Description:
+// * @Date: Create in 20:06 2020-09-08
+// */
+//public abstract class AbsDataHandler implements DataHandler {
+//
+//
+//    private String jsonStr;
+//
+//    public String getJsonStr() {
+//        return jsonStr;
+//    }
+//
+//    public void setJsonStr(String jsonStr) {
+//        this.jsonStr = jsonStr;
+//    }
+//
+//    void replace(JSONObject jsonObject, DataHandler dataHandler) {
+//        Set<String> keys = jsonObject.keySet();
+//        for (String key : keys) {
+//            Object obj = jsonObject.get(key);
+//            if (obj instanceof JSONObject) {
+//                replace((JSONObject) obj, dataHandler);
+//            } else {
+//                dataHandler.handler(jsonObject, key);
+//            }
+//        }
+//    }
+//
+//
+//
+//    @Override
+//    public void service() {
+//        JSONObject jsonObject=null;
+//        try {
+//            BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(filePath())));
+//            for (String line = br2.readLine(); line != null; line = br2.readLine()) {
+//                jsonObject = JSONObject.parseObject(line);
+//                replace(jsonObject,this);
+//                jsonStr=jsonObject.toJSONString();
+//                // getProducer().send(topic(), JSON.toJSONString(jsonObject, SerializerFeature.WriteMapNullValue), getImei());
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//
+//    }
+//
+//
+//
+//}
--- src/test/java/com/g7s/ics/utils/handler/CraeatOrderRepalce.java
+++ src/test/java/com/g7s/ics/utils/handler/CraeatOrderRepalce.java
@@ -1,25 +1,25 @@
-package com.g7s.ics.handler;
-
-import com.alibaba.fastjson.JSONObject;
-
-/**
- * @Author: zhangwenping
- * @Description:
- * @Date: Create in 19:34 2020-09-08
- */
-
-public class CraeatOrderRepalce extends AbsDataHandler {
-    @Override
-    public void handler(JSONObject jsonObject, String key) {
-        if ("code".equals(key)) {
-            jsonObject.put("code", "2020-08-03");
-        }
-    }
-
-        @Override
-        public String filePath() {
-            return "/Users/zhangwenping/Documents/make_data/ICS_API_Test/src/main/resources/muti/CreateOrder.txt";
-        }
-
-
-}
+//package com.g7s.ics.handler;
+//
+//import com.alibaba.fastjson.JSONObject;
+//
+///**
+// * @Author: zhangwenping
+// * @Description:
+// * @Date: Create in 19:34 2020-09-08
+// */
+//
+//public class CraeatOrderRepalce extends AbsDataHandler {
+//    @Override
+//    public void handler(JSONObject jsonObject, String key) {
+//        if ("code".equals(key)) {
+//            jsonObject.put("code", "2020-08-03");
+//        }
+//    }
+//
+//        @Override
+//        public String filePath() {
+//            return "/Users/zhangwenping/Documents/make_data/ICS_API_Test/src/main/resources/muti/CreateOrder.txt";
+//        }
+//
+//
+//}
